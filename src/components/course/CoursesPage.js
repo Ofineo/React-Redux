ac7a21228894a78b944a43e6898da465b4f11f14
Jordi <ofineo@gmail.com>
2018-05-24 20:51:05 +0100

    fix: CoursesPage this binding problem

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -1,35 +1,42 @@
-import React, {PropTypes} from 'react';
+import React, { PropTypes } from 'react';
 
 
 class CoursesPage extends React.Component {
-    constructor(props, context) {
-        super(props, context);
+  constructor(props, context) {
+    super(props, context);
+    this.state = {
+      course: { title: '' }
+    };
+    this.onTitleChange= this.onTitleChange.bind(this);
+    this.onClickSave= this.onClickSave.bind(this);
+  }
 
-        this.state={
-            course:{title: null}
-        };
-    }
+  onTitleChange(event) {
+    let course = this.state.course;
+    course.title = event.target.value;
+    this.setState({ course: course });
+  }
 
-    onTitleChange(event){
-        const course =this.state.course;
-        course.title = event.target.value;
-        this.setState({course: course});
-    }
+  onClickSave(){
+    alert(`Saving ${this.state.course.title}`);
+  }
+
 
   render() {
+    //const { courses } = this.props;
 
     return (
       <div>
         <h1>Courses</h1>
-        <h2>Add Course</h2>
         <input
-        type="text"
-        onChange={this.onTitleChange()}
-        value={this.state.course.title}/>
+          type="text"
+          onChange={this.onTitleChange}
+          value={this.state.course.title}
+        />
         <input
-        type="submit"
-        value="Save"
-        onClick={this.onClickSave()}/>
+          type="submit"
+          value="Save"
+          onClick={this.onClickSave} />
       </div>
     );
   }
