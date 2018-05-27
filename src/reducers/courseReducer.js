5fe373063b726277175a586523225c046d509df7
Jordi <ofineo@gmail.com>
2018-05-27 12:50:04 +0100

    feat: CoursesPage component specify dispatch properties in the connect function

--- src/reducers/courseReducer.js
+++ src/reducers/courseReducer.js
@@ -1,4 +1,4 @@
-export default function coursereducer(state = [], action) {
+export default function courseReducer(state = [], action) {
     switch (action.type) {
         case 'CREATE_COURSE':
             return [...state,
