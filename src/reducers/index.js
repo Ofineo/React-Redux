eac4b2725b4c4285896ff496beaa75c334c86ad9
Jordi <ofineo@gmail.com>
2018-06-02 18:43:49 +0100

    populate authors dropdown

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import {combineReducers} from 'redux';
 import courses from './courseReducer';
+import authors from './authorReducer';
 
 const rootReducer = combineReducers({
-    courses //shorthand property names ES6 courses: courses
+    courses, //shorthand property names ES6 courses: courses
+    authors
 });
 
 export default rootReducer;
