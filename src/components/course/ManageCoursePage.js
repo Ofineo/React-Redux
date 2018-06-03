c3bf387c299b6c94df0a845186f2978200168e71
Jordi <ofineo@gmail.com>
2018-06-03 11:05:36 +0100

    feat: updateCourseState function to update fields and state

--- src/components/course/ManageCoursePage.js
+++ src/components/course/ManageCoursePage.js
@@ -12,12 +12,21 @@ export class ManageCoursePage extends React.Component {
             course: Object.assign({}, props.course),
             errors: {}
         };
+        this.updateCourseState = this.updateCourseState.bind(this);
     }
 
-    render() {
+    updateCourseState(event){
+        const field = event.target.name;
+        let course = Object.assign({},this.state.course);
+        course[field] = event.target.value;
+        return this.setState({course});
+    }
+
+        render() {
         return (
             <CourseForm
                 allAuthors={this.props.authors}
+                onChange={this.updateCourseState}
                 course={this.state.course}
                 errors={this.state.errors}
             />
